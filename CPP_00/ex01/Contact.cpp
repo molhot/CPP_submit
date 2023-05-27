c295898f9da2044ec57b4801f628820a7ee793bb
molhot <sakata19991214@gmail.com>
2023-05-27 22:11:46 +0900

    CPP00 ex01 SEARCH command recover

--- CPP_00/ex01/Contact.cpp
+++ CPP_00/ex01/Contact.cpp
@@ -6,7 +6,7 @@
 /*   By: user <user@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 00:39:28 by mochitteiun       #+#    #+#             */
-/*   Updated: 2023/05/27 22:04:32 by user             ###   ########.fr       */
+/*   Updated: 2023/05/27 22:11:23 by user             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,24 +27,33 @@ bool	Contact::init(size_t index)
     this->_index = index + 1;
     std::cout << "input firstname" << std::endl;
 	std::getline(std::cin, this->_firstname);
+	std::cin.clear();
 	if (this->_firstname == "")
 		return (false);
+	
     std::cout << "input lastname" << std::endl;
 	std::getline(std::cin, this->_lastname);
+	std::cin.clear();
 	if (this->_lastname == "")
 		return (false);
+	
     std::cout << "input nickname" << std::endl;
 	std::getline(std::cin, this->_nickname);
+	std::cin.clear();
 	if (this->_nickname == "")
 		return (false);
+	
     std::cout << "input phonenumber" << std::endl;
 	std::getline(std::cin, this->_phonenumber);
+	std::cin.clear();
 	if (this->_phonenumber == "")
 		return (false);
 	if (ft_isdigit(this->_phonenumber) == false)
 		return (false);
+	
     std::cout << "input secret" << std::endl;
 	std::getline(std::cin, this->_secret);
+	std::cin.clear();
 	if (this->_secret == "")
 		return (false);
 	return (true);
@@ -71,7 +80,7 @@ void    Contact::content_show(void)
     std::cout << this->_index;
 	std::cout << " | ";
 	show_wd_eight(this->_firstname);
+	show_wd_eight(this->_lastname);
 	show_wd_eight(this->_nickname);
-	show_wd_eight(this->_phonenumber);
 	std::cout << std::endl;
 }
