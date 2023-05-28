c8e65d226a1f91aba4f838da1afe28754227c6f0
molhot <sakata19991214@gmail.com>
2023-05-28 11:41:12 +0900

    CPP02 ex01 done

--- CPP_02/ex02/main.cpp
+++ CPP_02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: user <user@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/27 21:41:16 by mochitteiun       #+#    #+#             */
-/*   Updated: 2023/04/29 20:21:22 by user             ###   ########.fr       */
+/*   Updated: 2023/05/28 11:40:42 by user             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,10 @@ int	main( void ) {
 	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
 
 	std::cout << "boolean test " << (a>b) << std::endl;
+	if (a > b == 1)
+		std::cout << "this is true" << std::endl;
+	else
+		std::cout << "this is false" << std::endl;
 	std::cout << "a+a test " << (a+a) << std::endl;
 	
 	FixNumber e;
@@ -40,6 +44,8 @@ int	main( void ) {
 	std::cout << e++ << std::endl;
 	std::cout << e << std::endl;
 	std::cout << f << std::endl;
+
 	std::cout << FixNumber::max( e, f ) << std::endl;
-return 0;
+	
+	return 0;
 }
